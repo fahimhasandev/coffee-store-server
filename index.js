f72b51e2790a2b57fa1b75f69117aac57f4a86a6
fahimhasandev <fhasan5@wgu.edu>
2024-12-02 15:47:52 -0600

    new files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require("cors")
 require("dotenv").config()
 const port = process.env.PORT || 5001
-const { MongoClient, ServerApiVersion } = require("mongodb")
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb")
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.uida1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 //Middleware
@@ -23,14 +23,58 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect()
-
     const coffeeCollection = client.db("coffeeDB").collection("coffee")
 
+    //POST - create
     app.post("/coffee", async (req, res) => {
       const newCoffee = req.body
       console.log(newCoffee)
 
       const result = await coffeeCollection.insertOne(newCoffee)
+      res.send(result)
+    })
+
+    //GET
+    app.get("/coffee", async (req, res) => {
+      const cursor = await coffeeCollection.find().toArray()
+      res.send(cursor)
+    })
+
+    //Single GET ITems
+    app.get("/coffee/:id", async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await coffeeCollection.findOne(query)
+      res.send(result)
+    })
+
+    //Delete
+    app.delete("/coffee/:id", async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await coffeeCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    //update
+    app.put("/coffee/:id", async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updatedCoffee = req.body
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          taste: updatedCoffee.taste,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo
+        }
+      }
+
+      const result = await coffeeCollection.updateOne(filter, coffee, options)
       res.send(result)
     })
 
@@ -52,3 +96,19 @@ app.get("/", (req, res) => {
 app.listen(port, (req, res) => {
   console.log(`Server is listening on ${port}`)
 })
+
+// custom middleware
+// const logger = (req, res, next) => {
+//   console.log("Hi")
+//   next()
+// }
+
+// app.post('/add-data,logger, (req, res) => {
+// const data = req.body
+// console.log(data)
+// res.json({
+//   status: true,
+//   data: data
+// })
+//})
+// app.use(express.json()) --> req.body converts into json
