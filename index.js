06fd3962fd5e9282ef60af3092d601a372849995
fahimhasandev <fhasan5@wgu.edu>
2024-12-03 17:30:08 -0600

    vercel server setup

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
 const cors = require("cors")
-require("dotenv").config()
 const port = process.env.PORT || 5001
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb")
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.uida1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 //Middleware
-app.use(
-  cors({
-    origin: ["https://coffee-store-server-peach-three.vercel.app/", "https://coffee-store-ae874.web.app/"]
-  })
-)
+app.use(cors())
 app.use(express.json())
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect()
+    //await client.connect()
     const coffeeCollection = client.db("coffeeDB").collection("coffee")
     const UserCollection = client.db("coffeeDB").collection("usrs")
 
@@ -126,8 +122,8 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 })
-    console.log("Pinged your deployment. You successfully connected to MongoDB!")
+    //await client.db("admin").command({ ping: 1 })
+    //console.log("Pinged your deployment. You successfully connected to MongoDB!")
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close()
